52d31900dbac106169ba12ff1e3b89940666f3b4
PeterTheOne <petertheone@gmail.com>
2018-04-26 18:27:05 +0200

    add accounts.txt and write some functions

--- fetch.py
+++ fetch.py
@@ -1,18 +1,30 @@
 import tweepy
 import config
-# todo: fix requirements
-
-auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-auth.set_access_token(config.access_token, config.access_token_secret)
-
-api = tweepy.API(auth)
 
 
-users = ['@HCStracheFP', '@sebastiankurz']
-for user in users:
-    public_tweets = api.user_timeline(user, count=100)
+def connectApi():
+    auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.set_access_token(config.access_token, config.access_token_secret)
+    return tweepy.API(auth)
+
+
+def getAccounts():
+    return open('accounts.txt').read().splitlines()
+
+
+def fetchTimeline(user):
+    return api.user_timeline(user, count=100)
+
+
+def saveTimeline(tweets, user):
     with open('data/timeline_{}.txt'.format(user), 'a') as tf:
-        for tweet in public_tweets:
-            print(tweet.text)
+        for tweet in tweets:
             tf.write(tweet.text + "\n")
+
+
+api = connectApi()
+users = getAccounts()
+for user in users:
+    tweets = fetchTimeline(user)
+    saveTimeline(tweets, user)
 
